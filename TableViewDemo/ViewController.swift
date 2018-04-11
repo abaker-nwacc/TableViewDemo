408a166e5f8cdaf5012470b902123e83f81f0eea
Allen Baker <abaker@nwacc.edu>
2018-04-11 10:44:34 -0500

    Initial commit. Includes data file loading in Main VC and incomplete setup of the TableViewController.

--- TableViewDemo/ViewController.swift
+++ TableViewDemo/ViewController.swift
@@ -9,10 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var boyNames: [String] = []
+    var girlNames: [String] = []
+    
+    @IBOutlet weak var boyNamesLabel: UILabel!
+    @IBOutlet weak var girlNamesLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Load the data files into local arrays
+        if let txtPath=Bundle.main.path(forResource: "GirlNames", ofType: "txt"){
+            do{
+                let names = try String(contentsOfFile: txtPath, encoding: .utf8).split(separator:"\r\n")
+                for n in names {
+                    girlNames.append(String(n))
+                }
+                girlNamesLabel.text = "Read \(girlNames.count) Girl names."
+            }catch{
+                girlNamesLabel.text = "Could not read GirlNames.txt"
+            }
+        }
+        if let txtPath=Bundle.main.path(forResource: "BoyNames", ofType: "txt"){
+            do{
+                let names = try String(contentsOfFile: txtPath, encoding: .utf8).split(separator:"\r\n")
+                for n in names {
+                    boyNames.append(String(n))
+                }
+                boyNamesLabel.text = "Read \(boyNames.count) Boy names."
+            }catch{
+                boyNamesLabel.text = "Could not read BoyNames.txt"
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +47,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let dest = segue.destination as! TableTableViewController
+        dest.boys = boyNames
+        dest.girls = girlNames
+    }
 
 }
 
