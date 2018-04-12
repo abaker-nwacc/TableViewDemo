0fcd27a2a552aabb0fd08662e4bc715bbd625d0e
Allen Baker <abaker@nwacc.edu>
2018-04-12 10:16:57 -0500

    Updated TableViewController delegate methods

--- TableViewDemo/TableTableViewController.swift
+++ TableViewDemo/TableTableViewController.swift
@@ -12,10 +12,20 @@ class TableTableViewController: UITableViewController {
 
     var girls:[String] = []
     var boys:[String] = []
+    var both:[(Int,String)] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = "Baby Names"
+        
+        for n in girls{
+            both.append((0,n))
+        }
+        for n in boys{
+            both.append((1,n))
+        }
+        both.sort(by: {(n1,n2)->Bool in n1.1<n2.1})
+        
         
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -32,44 +42,72 @@ class TableTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 2    // Section 0 = Girls, Section 1 = Boys
+        return 1    // Section 0 = Girls, Section 1 = Boys
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        var result = 0;     // Number of rows
-        switch section {
-        case 0:     // Girls
-            result = girls.count
-        case 1:     // Boys
-            result = boys.count
-        default:    // Anything else
-            result = 0
-        }
-        return result
+//        var result = 0;     // Number of rows
+//        switch section {
+//        case 0:     // Girls
+//            result = girls.count
+//        case 1:     // Boys
+//            result = boys.count
+//        default:    // Anything else
+//            result = 0
+//        }
+//        return result
+        return both.count
     }
 
-    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        var result = ""
-        switch section {
-        case 0:     // Girls
-            result = "Popular Girl's Names"
-        case 1:     // Boys
-            result = "Popular Boy's Names"
-        default:
-            result = ""
-        }
-        return result
-    }
+//    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//        var result = ""
+//        switch section {
+//        case 0:     // Girls
+//            result = "Popular Girl's Names"
+//        case 1:     // Boys
+//            result = "Popular Boy's Names"
+//        default:
+//            result = ""
+//        }
+//        return result
+//    }
+//
+//    override func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+//        return "End of Section"
+//    }
     
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        var cell: UITableViewCell
+//        switch indexPath.section {
+//        case 0:     // Girls
+//            cell = tableView.dequeueReusableCell(withIdentifier: "girlNameCell", for: indexPath)
+//            cell.textLabel?.text = girls[indexPath.row]
+//        case 1:     // Boys
+//            cell = tableView.dequeueReusableCell(withIdentifier: "boyNameCell", for: indexPath)
+//            cell.textLabel?.text = boys[indexPath.row]
+//        default:    // Will never happen
+//            cell = tableView.dequeueReusableCell(withIdentifier: "boyNameCell", for: indexPath)
+//            cell.textLabel?.text = "no name found"
+//        }
 
-        // Configure the cell...
-
+        let kidName = both[indexPath.row]
+        if kidName.0 == 0 {
+            cell = tableView.dequeueReusableCell(withIdentifier: "girlNameCell", for: indexPath)
+            cell.textLabel?.text = kidName.1
+        }else if kidName.0 == 1 {
+            cell = tableView.dequeueReusableCell(withIdentifier: "boyNameCell", for: indexPath)
+            cell.textLabel?.text = kidName.1
+        }else{
+            cell = tableView.dequeueReusableCell(withIdentifier: "boyNameCell", for: indexPath)
+            cell.backgroundColor = UIColor.red
+            cell.textLabel?.text = "no name found"
+        }
+        
+        
+        
         return cell
     }
-    */
+
 
     /*
     // Override to support conditional editing of the table view.
